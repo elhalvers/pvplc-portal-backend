b6fa3ae5b174af72a18cbae8635b279a21aab7f0
Tim <timofeyobraztsov@gmail.com>
2022-06-29 04:16:15 -0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://pvplc-portal-client.vercel.app"] }));
 app.use(morgan("tiny"));
 app.use(cookieParser());
 
